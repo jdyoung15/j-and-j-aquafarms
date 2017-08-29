1f4dc04fb69b48d09db806e5c9b8bdf252470846
Jeremy Young <jedayoung@gmail.com>
2017-08-29 10:45:26 -0500

    Replaced js files with minified versions, now calling js cdn sources as primary, now using json for aquaponics image data instead of html

--- assets/scripts/galleria-setup.js
+++ assets/scripts/galleria-setup.js
@@ -1,17 +1,30 @@
 (function() { 
   Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
 
-  Galleria.run('.galleria', {
-		thumbnails: false,
-		_toggleInfo: false
-	});
+  function runGalleriaInstance(instanceNum) {
+    const name = `galleria-${instanceNum}`;
+    const className = `.${name}` 
 
-  /*
-	Galleria.ready(function() {
-		this.attachKeyboard({
-  	left: this.prev,
-  	right: this.next
-		});
-	});
-  */
+    Galleria.run(className, {
+      thumbnails: false,
+      _toggleInfo: false,
+      dataSource: galleriaImgData[name], // galleriaImgData is specified in javascript of each aquaponics page
+      preload: 1
+    });
+  }
+
+  const numGalleriaInstances = $('.galleria').length;
+  for (var i = 1; i < numGalleriaInstances + 1; i++) {
+    runGalleriaInstance(i);
+  }
+
+  if (numGalleriaInstances === 1) {
+    Galleria.ready(function() {
+      this.attachKeyboard({
+      left: this.prev,
+      right: this.next
+      });
+    });
+  }
+
 }());
