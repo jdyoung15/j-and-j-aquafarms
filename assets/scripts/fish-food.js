061501dce7fb867aac0a6ffff3e224c229a964b7
Jeremy Young <jedayoung@gmail.com>
2020-12-03 20:57:20 -0800

    Change fish food ingredients to modal dialog for mobile; fix style issues

--- assets/scripts/fish-food.js
+++ assets/scripts/fish-food.js
@@ -6,25 +6,22 @@ $(".modal-dialog").dialog({
 });
 
 $(".opener").click(function() {
-  // If desktop mode, show ingredients popup instead of ingredients html page.
-  if (screen.width >= 960) {
-    var ffId = $(this).data('id');
-    var dialogId = `#${ffId}-dialog`;
-    $(dialogId).dialog("open");
+  var ffId = $(this).data('id');
+  var dialogId = `#${ffId}-dialog`;
+  $(dialogId).dialog("open");
 
-    // Close dialog when user clicks the "x" button
-    $(".btn-close").click(function() {
-      $(dialogId).dialog("close");
-    });
-    
-    // Close dialog when user clicks outside the dialog box
-    $(".ui-widget-overlay").click(function() {
-      $(dialogId).dialog("close");
-    });
+  // Close dialog when user clicks the "x" button
+  $(".btn-close").click(function() {
+    $(dialogId).dialog("close");
+  });
+  
+  // Close dialog when user clicks outside the dialog box
+  $(".ui-widget-overlay").click(function() {
+    $(dialogId).dialog("close");
+  });
 
-    // Prevent page from reloading to ingredients page
-    return false;
-  }
+  // Prevent page from reloading to ingredients page
+  return false;
 }); 
 
 // Update fish food listings with selected filter options
