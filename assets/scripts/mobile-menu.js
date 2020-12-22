bd36188154de10af7ad144830b88dec15086c3ac
Jeremy Young <jedayoung@gmail.com>
2020-12-21 20:54:53 -0800

    Rename nav links; fix incorrect mobile menu naming

--- assets/scripts/mobile-menu.js
+++ assets/scripts/mobile-menu.js
@@ -1,22 +1,22 @@
-// Show the mobile nav menu if the menu icon is clicked
-const menu = document.querySelector(".mobile-menu-icon")
+// Show the mobile nav menu if the hamburger menu icon is clicked
+const menu = document.querySelector(".hamburger-menu-icon")
 const body = document.querySelector("body")
 const nav = document.querySelector(".nav");
 menu.onclick = function(event) {
   nav.style.display = "block";
   menu.classList.add("clicked")
-  body.classList.add("mobile-menu-open");  
+  body.classList.add("hamburger-menu-open");  
 }
 
 // Hide the mobile nav menu if it's currently open and user has clicked outside the menu
 body.onclick = function(event) {
   if (menu.classList.contains("clicked")
-    && event.target.closest(".mobile-menu-icon") === null 
+    && event.target.closest(".hamburger-menu-icon") === null 
     && event.target.closest(".nav") === null) 
   {
     nav.style.display = "none";
     menu.classList.remove("clicked");  
-    body.classList.remove("mobile-menu-open");  
+    body.classList.remove("hamburger-menu-open");  
   }
 }
 
